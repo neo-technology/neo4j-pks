7c5bc96920027a96f1e2da36d6ede528557709a7
Moxious <moxious@oldhat.org>
2019-04-27 16:40:00 -0400

    test container first working version

---
+++ run-test-container.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+export IMAGE=gcr.io/neo4j-pivotal/causal-cluster/tester:3.5.4
+export NAMESPACE=default
+export HTTP_PORT=7474
+export HTTPS_PORT=7473
+export BOLT_PORT=7687
+export NAME=testrun
+export CORES=3
+export READ_REPLICAS=1
+
+cat test/tester.yaml | envsubst > target/tester.yaml
+
+# Delete pod if it happens to be leftover from a previous test run.
+kubectl delete pod testrun-tester --ignore-not-found=true
+
+# Kick off new test.
+kubectl apply -f target/tester.yaml --namespace $NAMESPACE
+
+if [ $? -ne 0 ] ; then
+   echo "Failed to run test container"
+   exit 1
+fi
+
+result=null
+
+while true
+do
+    result=$(kubectl get pod $NAME-tester --namespace $NAMESPACE --output=json | jq -r '.status.containerStatuses[0].state.terminated.reason')
+
+    if [ "$result" != "null" ] ; then
+        break
+    else 
+        echo "Test container is creating or still running.  Waiting..."
+    fi
+
+    sleep 4
+done
+
+echo "Dumping logs"
+kubectl logs $NAME-tester --namespace $NAMESPACE
+
+echo "=========================================="
+echo "FINAL TEST POD STATUS IS $result"
+echo "=========================================="
+
+if [ "$result" = "Error" ] ; then
+    echo "Tests failed"
+    exit 1
+fi
+
+if [ "$result" = "Completed" ] ; then
+    echo "Tests succeeded, mazel tov\!"
+    exit 0
+fi
+
+echo "Uncertain test pod result $result"
+exit 1
